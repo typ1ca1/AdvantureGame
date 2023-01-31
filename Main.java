722cb98633da64a20ef1516277f81e87ba9fbb74
Arystan <yarikmen12@gmail.com>
2023-01-31 09:56:48 -0500

    Enemy classes added

--- Main.java
+++ Main.java
@@ -4,5 +4,11 @@ public class Main {
         Weapon magicSword = new Weapon("Magic Sword", "qwe", 20, 50);
         Weapon softPillow = new Weapon("Soft Pillow", "A super soft pillow", 3, 1);
         Weapon bigMace = new Weapon("A big mace", "Biggest mace in the world", 10, 20);
+        Enemy greenOgre = new Enemy("Green Ogre", 30, 25);
+        Enemy redOgre = new Enemy("Red Ogre", 35, 30);
+        Enemy slowZombie = new Enemy("Slow Zombie", 20, 35);
+        Enemy fastZombie = new Enemy("Fast Zombie", 50, 10);
+        Enemy spotDog = new Enemy("Spot Dog", 20, 15);
+        Enemy bigSkeleton = new Enemy("Big Skeleton", 40, 20);
     }
 }
